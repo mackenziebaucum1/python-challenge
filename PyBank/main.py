506f83491d63fb77411012a08b9a7b39daabdde9
Mackenzie Baucum <mbaucum@mac.com>
2020-08-27 21:38:26 -0400

    updated code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,31 +1,30 @@
-#Import OS Module
+#Import  Modules
 
 import os
+import csv
 
 #Import CSV file
-
-import csv
 
 budget_data = os.path.join("Resources", "budget_data.csv")
 
 #Open and Read CSV file
 
-with open("budget_data") as csvfile:
+with open(budget_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
 
 # Read header row
 
-print(f"Header: Financial Analysis {csv_header}")
+    print(f"Header: Financial Analysis {csv_header}")
 
 #Read Each Row After Header 
 
-Profit = []
-months = []
+    Profit = []
+    months = []
 
-for row in csvreader:
-    Profit.append(int(rows[1]))
-    months.append(rows[0])
+    for row in csvreader:
+        Profit.append(int(row[1]))
+        months.append(row[0])
 
 #The net total amount of "Profit/Losses" over the entire period
 
@@ -54,17 +53,17 @@ greatest_decrease = min(revenue_change)
 
 print("Financial Analysis")
 
-    print("....................................................................................")
+print("....................................................................................")
 
-    print("total months: " + str(total_months))
+print("total months: " + str(total_months))
 
-    print("Total: " + "$" + str(sum(Profit)))
+print("Total: " + "$" + str(sum(Profit)))
 
-    print("Average change: " + "$" + str(revenue_average))
+print("Average change: " + "$" + str(revenue_average))
 
-    print("Greatest Increase in Profits: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase))
+print("Greatest Increase in Profits: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase))
 
-    print("Greatest Decrease in Profits: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease))
+print("Greatest Decrease in Profits: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease))
 
 #Export to text file
 
@@ -76,12 +75,10 @@ file.write("....................................................................
 
 file.write("total months: " + str(total_months) + "\n")
 
-file.write("Total: " + "$" + str(sum(P)) + "\n")
+file.write("Total: " + "$" + str(sum(Profit)) + "\n")
 
 file.write("Average change: " + "$" + str(revenue_average) + "\n")
 
 file.write("Greatest Increase in Profits: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase) + "\n")
 
 file.write("Greatest Decrease in Profits: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease) + "\n")
-
-file.close()
